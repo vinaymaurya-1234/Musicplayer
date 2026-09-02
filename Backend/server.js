126606b9ef4d8399b225bc1338a4e8e6afe35c3e
vinaymaurya-1234 <vinaymaurya11223344@gmail.com>
2026-09-02 18:09:06 +0530

    Fix Render port

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ const connectDB = require("./src/DB/DB");
 
 connectDB();
 
-App.listen(3000, () => {
-    console.log("Server running on port 3000.")
-})
+const PORT = process.env.PORT || 3000;
+
+App.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
